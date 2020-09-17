d75ba330a2c90d755827f7b6328d1e12449d650b
Abdulmonem Nafea <abdulmonemamin@hotmail.com>
2020-09-17 14:01:34 +1000

    add privacy page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Login from './componenets/Login'
 import Dashborad from './componenets/Dashboard'
 import Session from './componenets/Session'
+import Privacy from './componenets/Privacy'
+import Terms from './componenets/Terms.js'
 import Error from './componenets/Error'
 import CombinedContextProvider from './context/CombinedContext'
 import { CssBaseline } from '@material-ui/core'
@@ -19,6 +21,8 @@ function App() {
             <Route path='/' exact component={Login}/>
             <Route path='/dashboard' component={Dashborad} />
             <Route path='/session/:id' component={Session} />
+            <Route path='/privacy' component={Privacy} />
+            <Route path='/terms-and-conditions' component={Terms} />
             <Route component={Error} />
           </Switch>
         </Router>
